155fc65afae02d6e008701b4f1bfba9aeac998a1
LuppyBonkai <zluppen@gmail.com>
2017-06-26 19:44:42 -0500

    Update GravBox.py

--- GravBox.py
+++ GravBox.py
@@ -1,27 +1,29 @@
 from kivy.config import Config
-Config.set('graphics', 'width', '1000')
-Config.set('graphics', 'height', '500')
+Config.set('graphics', 'width', '1250')
+Config.set('graphics', 'height', '800')
 Config.set('graphics', 'resizable', 0)
-from kivy.app             import App
-from kivy.uix.widget      import Widget
-from kivy.uix.boxlayout   import BoxLayout
-from kivy.lang            import Builder
-from kivy.properties      import ObjectProperty
-from kivy.graphics        import Color, Ellipse, Line, Triangle, Rectangle
-from kivy.uix.button      import Button
-from kivy.uix.label       import Label
-from kivy.uix.slider      import Slider
-from kivy.uix.image       import Image
+from kivy.app                 import App
+from kivy.uix.widget          import Widget
+from kivy.uix.boxlayout       import BoxLayout
+from kivy.uix.relativelayout  import RelativeLayout
+from kivy.lang                import Builder
+from kivy.properties          import ObjectProperty
+from kivy.graphics            import Color, Ellipse, Line, Triangle, Rectangle
+from kivy.uix.button          import Button
+from kivy.uix.label           import Label
+from kivy.uix.slider          import Slider
+from kivy.uix.image           import Image
+from kivy.uix.effectwidget    import EffectWidget
+from kivy.clock               import Clock
+from kivy.cache               import Cache
 import numpy as np
 import random
+import threading
 
-red = [1,0,0,1]
-green = [0,1,0,1]
-blue =  [0,0,1,1]
-purple = [1,0,1,1]
 
 #Creates application
 class GravBox(App):
+    
     def build(self):
         
         #Creates main layout
@@ -29,13 +31,20 @@ class GravBox(App):
         size=(1000,500)
         
         #Creates left layout within main layout
-        LeftLayout=BoxLayout(padding=10)
+        LeftLayout=RelativeLayout(padding=10,size_hint=[.7,1])
         size=(700,500)
         layout.add_widget(LeftLayout)
         
+        Background_Image = Image(source='color_field_0.jpg')
+        LeftLayout.add_widget(Background_Image)
+        
+        Painter = DrawingEnvironment(size_hint=[.7,1])
+        size=(700,500)
+        LeftLayout.add_widget(Painter)
+        
         #Creates right layout within main layout, with vertical orientation
-        RightLayout=BoxLayout(padding=10,orientation='vertical')
-        size=(300,500)
+        RightLayout=BoxLayout(padding=10,orientation='vertical',size_hint=[.28,1])
+        size=(280,500)
         layout.add_widget(RightLayout)
         
         #Creates object menu button
@@ -85,12 +94,82 @@ class GravBox(App):
         #Go! Button
         GoButton = Button(text='Go!',font_size=40,bold=True)
         RightLayout.add_widget(GoButton)
+        
+
         return layout
+   
     
-class InteractiveEnvironment(Widget):
-    def build(self):
-        layout=BoxLayout(padding=10)
-        size=(500,500)
+#Creates the drawing environment which is called by the main application
+class DrawingEnvironment(Widget):
+    xlabel = ObjectProperty()
+    ylabel = ObjectProperty()
+    xlabel2 = ObjectProperty()
+    ylabel2 = ObjectProperty()
+    
+    #Provides functionality for a touch_down event
+    def on_touch_down(self, touch):
+        with self.canvas:
+            if touch.x < 880:
+                self.canvas.clear()
+                d = 10
+                Ellipse(pos=(touch.x - d/2, touch.y - d/2), size=(d,d))
+                touch.ud['line'] = Line(points=(touch.x, touch.y))
+                global x_init
+                global y_init
+                x_init = touch.x
+                y_init = touch.y
+                print x_init
+                print y_init
+            else:
+                self.canvas.clear()
+                print('touch_down out of bounds')
+
+    #Provides functionality for a touch_up event            
+    def on_touch_up(self, touch):
+        with self.canvas:
+            if touch.x < 880:
+                self.canvas.clear()
+                d = 10
+                Ellipse(pos=(touch.x - d/2, touch.y - d/2), size=(d,d))
+                touch.ud['line'] = Line(points=(touch.x, touch.y))
+                global x_diff
+                global y_diff
+                x_diff = (touch.x - x_init)
+                print touch.x
+                print touch.y
+                #print x_diff
+                y_diff = (touch.y - y_init)
+                #print y_diff
+                global angle
+                angle = np.arctan2(y_diff,x_diff)*180/np.pi
+                #print angle
+                Line(points=[x_init,y_init,touch.x,touch.y])
+            else:
+                self.canvas.clear()
+                print touch.x
+                print('touch_up out of bounds')
+            
+
+    #Provides functionality for a touch_move event            
+    def on_touch_move(self,touch):
+        with self.canvas:
+            if touch.x < 880:
+                self.canvas.clear()
+                d = 10
+                Ellipse(pos=(touch.x - d/2, touch.y - d/2), size=(d,d))
+                touch.ud['line'] = Line(points=(touch.x, touch.y))
+                global x_init
+                global y_init
+                global x_final
+                global y_final
+                global x_diff
+                global y_diff
+                x_final = touch.x
+                y_final = touch.y
+                Line(points=[x_init,y_init,touch.x,touch.y])
+            else:
+                self.canvas.clear()
+                print('touch_move out of bounds')
     
 if __name__ == "__main__":
     app=GravBox()
