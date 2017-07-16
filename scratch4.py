d0d164956043f3810f88bd0c0eddf29ccf11ca68
MasReed <bluekitdrummer@gmail.com>
2017-07-15 22:17:30 -0500

    Add files via upload

--- scratch4.py
+++ scratch4.py
@@ -9,6 +9,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.slider import Slider
+from kivy.uix.switch import Switch
 from kivy.graphics import Color, Rectangle, Ellipse, Line
 from random import randint
 
@@ -23,6 +24,7 @@ Config.set('graphics', 'resizeable', 1)  #makes the window resizeable if 1, stat
 Window.size = (1280, 800)
 Window.clearcolor = (0, 0, 0, 1.) #fixes drawing issues on some phones
 
+assetsdirectory = 'C://Users/Mason/Desktop/Sandbox Kivy/' #CURRENT FILEPATH FOR ALL IMAGES/ASSETS
 
 
 class MyButton(Button):
@@ -36,7 +38,7 @@ class WelcomeScreen(Screen):
     
     buttonList = ['START YOUR JOURNEY', 'ABOUT', 'UIOWA', 'BACK'] #list of buttons, used for naming 
     
-    welcomeImage = 'C://Users/Mason/Desktop/Sandbox Kivy/Gravity-Sandbox-homescreen.jpg' #WELCOME SCREEN BACKGROUND IMAGE PATH
+    welcomeImage = assetsdirectory+str('Gravity-Sandbox-homescreen.jpg') #WELCOME SCREEN BACKGROUND IMAGE PATH
     
     def __init__(self, **kwargs):
         super(WelcomeScreen, self).__init__(**kwargs)
@@ -96,7 +98,7 @@ class WelcomeScreen(Screen):
     
 class AboutScreen(Screen):
     
-    aboutImage = 'C://Users/Mason/Desktop/Sandbox Kivy/gray_star.png' #ABOUT SCREEN BACKGROUND IMAGE 
+    aboutImage = assetsdirectory+str('gray_star.png') #ABOUT SCREEN BACKGROUND IMAGE 
     
     def __init__(self, **kwargs):
         super(AboutScreen, self).__init__(**kwargs)
@@ -114,8 +116,9 @@ class AboutScreen(Screen):
         self.img.opacity = 0.4
         self.add_widget(self.img)
         
-        self.aboutText = Label(text= 'ABOUT INFO HERE')
-        self.aboutText.pos = (.25, .25)
+        self.aboutText = Label(text= 'GravBox is the interface application for the Augmented Reality (AR) Sandbox for gravitational dynamics simulations designed and built\n by Dr. Hai Fu\'s Introduction to Astrophysics class during the 2016-2017 academic year and beyond.\n GravBox itself was designed by Zachary Luppen, Erin Maier, and Mason Reed.\n\nAR Sandbox is the result of an NSF-funded project on informal science education for freshwater lake and watershed science developed by the\n UC Davis\' W.M. Keck Center for Active Visualization in the Earth Sciences (KeckCAVES),\n together with the UC Davis Tahoe Environmental Research Center, Lawrence Hall of Science, and ECHO Lake Aquarium and Science Center.')
+        self.aboutText.pos = (.5, .5)
+        #self.aboutText.size = self.size
         self.add_widget(self.aboutText)
                 
         tmpBtn1 = MyButton(text = 'BACK') #start button
@@ -132,7 +135,7 @@ class AboutScreen(Screen):
         
 class UiowaScreen(Screen):
     
-    uiowaImage  = 'C://Users/Mason/Desktop/Sandbox Kivy/gold_star.png' #UIOWA SCREEN BACKGROUND IMAGE
+    uiowaImage  = assetsdirectory+str('gold_star.png') #UIOWA SCREEN BACKGROUND IMAGE
     
     def __init__(self, **kwargs):
         super(UiowaScreen, self).__init__(**kwargs)
@@ -199,8 +202,6 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         self.leftlayout.x = 0 #Window.width - self.leftlayout.width
         self.leftlayout.y = 0 #Window.width/2 - self.leftlayout.height/2
         
-
-        
               
         with self.canvas: #sets canvas instructions for the float layout and draws a red rectangle filling the entire layout
             Color(1., 0, 0, .4) #RED
@@ -232,28 +233,33 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         self.rightlayout.add_widget(objButton)
         
         #TOPOGRAPHY TOGGLE SWITCH
-        
+        topoSwitch = Switch(active = True)
+        topoSwitch.pos_hint = {'x':0, 'y': .64}
+        topoSwitch.size_hint = (.3, .2)
+        topoSwitch.background_color = [.4, .4, .4, 1]
+        topoSwitch.bind(active = self.topoChange)
+        self.rightlayout.add_widget(topoSwitch)
         
         #SPEED LABEL
         self.spdLabel = Label(text='Speed: 50')
-        self.spdLabel.pos_hint = {'x':0, 'y':.71}
+        self.spdLabel.pos_hint = {'x':0, 'y':.57}
         self.spdLabel.size_hint = (.3, .1)
         self.spdLabel.background_color = [.4, .4, .4, 1]
         self.rightlayout.add_widget(self.spdLabel)
         
         #SPEED SLIDER & LABELS
         minLabel = Label(text='0')
-        minLabel.pos_hint = {'x':0, 'y':.58}
+        minLabel.pos_hint = {'x':0, 'y':.51}
         minLabel.size_hint = (.1, .1)
         self.rightlayout.add_widget(minLabel)
         
         maxLabel = Label(text='1.0')
-        maxLabel.pos_hint = {'x':.2, 'y':.58}
+        maxLabel.pos_hint = {'x':.2, 'y':.51}
         maxLabel.size_hint = (.1, .1)
         self.rightlayout.add_widget(maxLabel)
         
         spdSlider = Slider(min=0, max=1.0, step=0.01, value=.5)
-        spdSlider.pos_hint = {'x':.05, 'y':.54}
+        spdSlider.pos_hint = {'x':.05, 'y':.51}
         spdSlider.size_hint = (.2, .1)
         spdSlider.bind(value=self.spdSliderChange)
         self.rightlayout.add_widget(spdSlider)
@@ -261,24 +267,24 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         
         #ANGLE LABEL
         self.angLabel = Label(text='Angle: 180')
-        self.angLabel.pos_hint = {'x':0, 'y': .42}
+        self.angLabel.pos_hint = {'x':0, 'y': .33}
         self.angLabel.size_hint = (.3, .1)
         self.angLabel.background_color = [.4, .4, .4, 1]
         self.rightlayout.add_widget(self.angLabel)
         
         #ANGLE SLIDER & LABELS
         min2Label = Label(text='0')
-        min2Label.pos_hint = {'x':0, 'y':.28}
+        min2Label.pos_hint = {'x':0, 'y':.27}
         min2Label.size_hint = (.1, .1)
         self.rightlayout.add_widget(min2Label)
         
         max2Label = Label(text='360')
-        max2Label.pos_hint = {'x':.2, 'y':.28}
+        max2Label.pos_hint = {'x':.2, 'y':.27}
         max2Label.size_hint = (.1, .1)
         self.rightlayout.add_widget(max2Label)
         
         angSlider = Slider(min=0, max=360, step=0.01, value=180)
-        angSlider.pos_hint = {'x':.05, 'y':.24}
+        angSlider.pos_hint = {'x':.05, 'y':.27}
         angSlider.size_hint = (.2, .1)
         angSlider.bind(value=self.angSliderChange)
         self.rightlayout.add_widget(angSlider)
@@ -300,7 +306,7 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         self.add_widget(self.layout)
         
         #DEFAULT BACKGROUND IMAGE
-        self.bg_image_default = Image(source='C://Users/Mason/Downloads/starfield.jpg')
+        self.bg_image_default = Image(source=assetsdirectory+str('starfield.jpg'))
         self.bg_image_default.allow_stretch = True
         self.bg_image_default.keep_ratio = False
         self.bg_image_default.size_hint_x = .7
@@ -312,7 +318,7 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         
         #BACKGROUND IMAGE UPDATE FUNCTION
         #if self.manager.current == 'screen2':
-        Clock.schedule_interval(self.imageUpdate, 1.0/5.0) #seconds per update
+        #Clock.schedule_interval(self.imageUpdate, 1.0/5.0) #SCHEDULES THE IMAGE UPDATING TO OCCUR
                 
         
     def massSelect(self, *args):
@@ -320,14 +326,12 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         
     def userInput(self, *args): #This function to be used when the go button is relased, this will need input data from DrawingApp class
         print('UserInput Function Called')
-        #print('x_initial input: %s' %self.x_initial)
-        save_path = 'C://Users/Mason/Desktop/Sandbox Kivy/' #SAVE PATH OF ALGORITHM_INPUT.TXT
         name_of_file = 'algorithm_input'                    #USER INPUT FILENAME
-        inputFileName = os.path.join(save_path, name_of_file+'.txt') #TOTAL PATH NAME
+        inputFileName = os.path.join(assetsdirectory, name_of_file+'.txt') #TOTAL PATH NAME
         
         with open(inputFileName, 'w') as f:
 	        f.write('%f\t%f\t%f\t%f\t%f\t%f' % (drawUtility.x_initial, drawUtility.y_initial, drawUtility.x_final, drawUtility.y_final, drawUtility.x_delta, drawUtility.y_delta))
-        
+   
     def changer(self, *args):
         self.manager.current = 'screen1'
         
@@ -340,6 +344,9 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
     def spdSliderChange(self, instance, value):
         self.spdLabel.text = 'Speed: ' +str(value)
         
+    def topoChange(self, instance, value):
+        print('Topography toggled')
+        
     def imageUpdate(self, dt):
         
         #TESTING MULTIPLE IMAGE UPDATE
@@ -350,7 +357,7 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
             pass
             
         imageNumber = randint(1,4)
-        imageStr = 'C://Users/Mason/Desktop/Sandbox Kivy/sandstone_'+str(imageNumber)+'.png'
+        imageStr = assetsdirectory+str('sandstone_')+str(imageNumber)+'.png'
         self.bg_image = Image(source=imageStr)
         self.bg_image.allow_stretch = True
         self.bg_image.keep_ratio = False
@@ -358,13 +365,24 @@ class InteractionScreen(Screen): #This is the main screen for drawing and user i
         self.bg_image.size_hint_y = 1
         self.bg_image.pos = (0, 0)
         self.bg_image.opacity = 0.6
+        #self.bg_image.reload()
         
         self.leftlayout.add_widget(self.bg_image)
         
-        print('The background image has updated')
+        #print('The background image has updated')
+        
 
+class DataImport(Widget): #NOT SURE IF THIS IS FEASIBLE FOR DATA INPUT
+    
+    def __init__(self, **kwargs):
+        super(DataImport, self).__init__(**kwargs)
         
+    def fileToList(self, instance, value):
         
+        with open('algorithm_output.txt') as f:
+            animList = f.readlines()
+            print (animList)
+                
         
 class DrawingApp(Widget):
     
@@ -409,7 +427,7 @@ class DrawingApp(Widget):
             
             if touch.x > self.x_bounds:
                 self.out_of_bounds = True
-                print ('Touch down event out of bounds with x > %s \n' %(self.x_bounds))
+                #print ('Touch down event out of bounds with x > %s \n' %(self.x_bounds))
                 pass	        
 	        
             elif touch.x < self.x_bounds:
@@ -479,7 +497,7 @@ class DrawingApp(Widget):
                 Line(points=[self.x_initial, self.y_initial, touch.x, touch.y])
             
             elif touch.x > self.x_bounds and self.out_of_bounds == True:
-                print('x is greater than x_bounds and touch up out of bounds is true')
+                #print('x is greater than x_bounds and touch up out of bounds is true')
                 pass
                 
             elif touch.x <= self.x_bounds and self.out_of_bounds == True:
@@ -500,7 +518,6 @@ class TestApp(App):
         sm.add_widget(sc2)
         sm.add_widget(sc3)
         sm.add_widget(sc4)
-        #Clock.schedule_interval(sc2.imageUpdate, 1.0/60.0)
         print (sm.screen_names)
         return sm
         
